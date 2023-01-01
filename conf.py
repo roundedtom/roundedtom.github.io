00c7d871de8b11c34b6730c644feb03015fdc8b4
roundedtom <youensss@foxmail.com>
2023-01-01 10:57:55 +0800

    Nikola auto commit.
    Nikola version: 8.2.3

--- conf.py
+++ conf.py
@@ -142,7 +142,7 @@ TRANSLATIONS_PATTERN = '{path}.{lang}.{ext}'
 
 NAVIGATION_LINKS = {
     DEFAULT_LANG: (
-        ("/archive.html", "Archive"),
+        # ("/archive.html", "Archive"),
         ("/categories/", "Categories & Tags"),
         # ("/rss.xml", "RSS feed"),
     ),
@@ -1102,7 +1102,7 @@ PRETTY_URLS = True
 
 # Want to use KaTeX instead of MathJax? While KaTeX may not support every
 # feature yet, it's faster and the output looks better.
-USE_KATEX = False
+USE_KATEX = True
 
 # KaTeX auto-render settings. If you want support for the $.$ syntax (which may
 # conflict with running text!), just use this config:
